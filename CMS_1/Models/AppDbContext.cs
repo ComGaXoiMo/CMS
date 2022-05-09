058615234bedb7c20ee74661ee603e385fadc418
ComGaXoiMo <vietng50@gmail.com>
2022-05-09 12:19:49 +0700

    CMS_1

---
+++ CMS_1/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CMS_1.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ CMS_1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMS_1.Models
+{
+    [Table(name: "User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
